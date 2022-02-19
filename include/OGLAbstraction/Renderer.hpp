7951c5b15d7f26a5764b0a713992c8bc3fec6f32
FlowingSkiff <boats303@gmail.com>
2022-02-19 16:52:04 -0700

    Changed renderer to static

--- include/OGLAbstraction/Renderer.hpp
+++ include/OGLAbstraction/Renderer.hpp
@@ -4,9 +4,9 @@
 #include "Shader.hpp"
 class Renderer
 {
-public:
-    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& s) const;
-    void Clear(const glm::vec4 color = {0.2f, 0.3f, 0.3f, 1.0f}) const;
-private:
+  public:
+    static void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& s);
+    static void Clear(const glm::vec4 color = { 0.2f, 0.3f, 0.3f, 1.0f });
 
+  private:
 };
--- src/OGLAbstraction/Renderer.cpp
+++ src/OGLAbstraction/Renderer.cpp
@@ -1,6 +1,6 @@
 #include "Renderer.hpp"
 
-void Renderer::Draw(const VertexArray& vao, const IndexBuffer& ib, const Shader& shader) const
+void Renderer::Draw(const VertexArray& vao, const IndexBuffer& ib, const Shader& shader)
 {
     shader.Bind();
     vao.Bind();
@@ -8,7 +8,7 @@ void Renderer::Draw(const VertexArray& vao, const IndexBuffer& ib, const Shader&
     glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
 }
 
-void Renderer::Clear(glm::vec4 color) const
+void Renderer::Clear(glm::vec4 color)
 {
     glClearColor(color[0], color[1], color[2], color[3]);
     glClear(GL_COLOR_BUFFER_BIT);
