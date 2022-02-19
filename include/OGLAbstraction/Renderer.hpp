334e6714d644a6dc647ee4effa6a379a31ccb53d
FlowingSkiff <boats303@gmail.com>
2022-02-19 16:54:35 -0700

    Const color ref

--- include/OGLAbstraction/Renderer.hpp
+++ include/OGLAbstraction/Renderer.hpp
@@ -6,7 +6,7 @@ class Renderer
 {
   public:
     static void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& s);
-    static void Clear(const glm::vec4 color = { 0.2f, 0.3f, 0.3f, 1.0f });
+    static void Clear(const glm::vec4& color = { 0.2f, 0.3f, 0.3f, 1.0f });
 
   private:
 };
--- src/OGLAbstraction/Renderer.cpp
+++ src/OGLAbstraction/Renderer.cpp
@@ -8,7 +8,7 @@ void Renderer::Draw(const VertexArray& vao, const IndexBuffer& ib, const Shader&
     glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
 }
 
-void Renderer::Clear(glm::vec4 color)
+void Renderer::Clear(const glm::vec4& color)
 {
     glClearColor(color[0], color[1], color[2], color[3]);
     glClear(GL_COLOR_BUFFER_BIT);
