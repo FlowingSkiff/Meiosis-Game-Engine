f8fb064665bf8e64493e23bf8822b7137a18c67a
FlowingSkiff <boats303@gmail.com>
2022-02-21 23:53:46 -0700

    fixed random generator

--- src/Util/Random.cpp
+++ src/Util/Random.cpp
@@ -11,7 +11,7 @@ float randF()
 double randD()
 {
     static std::random_device device;
-    static std::mt19937 generator(device);
+    static std::mt19937 generator(device());
     static std::uniform_real_distribution<> distribution(0.0, 1.0);
     return distribution(generator);
 }
