956f08fb64abee782d6951c5423f063299725149
FlowingSkiff <boats303@gmail.com>
2022-02-22 22:14:24 -0700

    basic start of scene class

---
+++ include/Application/Scene.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+class Scene
+{
+  private:
+    // Model:
+    // models have the index, vertices, and textures, colors, etc.
+    // std::vector<Model> m_models;
+    //
+};
