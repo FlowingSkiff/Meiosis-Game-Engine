466931494d23080512b77dcf6e661f8b6fd0efc2
FlowingSkiff <boats303@gmail.com>
2022-03-01 22:20:26 -0700

    Changed location of log files

--- include/Genesis/Core/Log.hpp
+++ include/Genesis/Core/Log.hpp
@@ -11,6 +11,8 @@ class GN_API Logger
 {
   public:
     static void init();
+
+  public:
     template<typename... ARGS>
     static void warn(std::string_view str, const ARGS&... args);
     template<typename... ARGS>
@@ -22,8 +24,22 @@ class GN_API Logger
     template<typename... ARGS>
     static void error(std::string_view str, const ARGS&... args);
 
+  public:
+    template<typename... ARGS>
+    static void coreWarn(std::string_view str, const ARGS&... args);
+    template<typename... ARGS>
+    static void coreInfo(std::string_view str, const ARGS&... args);
+    template<typename... ARGS>
+    static void coreDebug(std::string_view str, const ARGS&... args);
+    template<typename... ARGS>
+    static void coreTrace(std::string_view str, const ARGS&... args);
+    template<typename... ARGS>
+    static void coreError(std::string_view str, const ARGS&... args);
+
+
   private:
     static std::shared_ptr<spdlog::logger> clientLog;
+    static std::shared_ptr<spdlog::logger> coreLog;
 };
 
 template<typename... ARGS>
@@ -57,20 +73,65 @@ void Logger::error(std::string_view str, const ARGS&... args)
     clientLog->error(str, args...);
 }
 
+template<typename... ARGS>
+void Logger::coreWarn(std::string_view str, const ARGS&... args)
+{
+    coreLog->warn(str, args...);
+}
+
+template<typename... ARGS>
+void Logger::coreDebug(std::string_view str, const ARGS&... args)
+{
+    coreLog->debug(str, args...);
+}
+
+template<typename... ARGS>
+void Logger::coreTrace(std::string_view str, const ARGS&... args)
+{
+    coreLog->trace(str, args...);
+}
+
+template<typename... ARGS>
+void Logger::coreInfo(std::string_view str, const ARGS&... args)
+{
+    coreLog->info(str, args...);
+}
+
+
+template<typename... ARGS>
+void Logger::coreError(std::string_view str, const ARGS&... args)
+{
+    coreLog->error(str, args...);
+}
+
 
 }// namespace Genesis
 
 
 #ifdef ENABLE_LOG
+
 #define ENGINE_WARN(...) Genesis::Logger::warn(__VA_ARGS__)
 #define ENGINE_DEBUG(...) Genesis::Logger::debug(__VA_ARGS__)
 #define ENGINE_TRACE(...) Genesis::Logger::trace(__VA_ARGS__)
 #define ENGINE_INFO(...) Genesis::Logger::info(__VA_ARGS__)
 #define ENGINE_ERROR(...) Genesis::Logger::error(__VA_ARGS__)
+
+#define GN_ENGINE_WARN(...) Genesis::Logger::coreWarn(__VA_ARGS__)
+#define GN_ENGINE_DEBUG(...) Genesis::Logger::coreDebug(__VA_ARGS__)
+#define GN_ENGINE_TRACE(...) Genesis::Logger::coreTrace(__VA_ARGS__)
+#define GN_ENGINE_INFO(...) Genesis::Logger::coreInfo(__VA_ARGS__)
+#define GN_ENGINE_ERROR(...) Genesis::Logger::coreError(__VA_ARGS__)
 #else
+
 #define ENGINE_WARN(...)
 #define ENGINE_DEBUG(...)
 #define ENGINE_TRACE(...)
 #define ENGINE_INFO(...)
 #define ENGINE_ERROR(...)
+
+#define GN_ENGINE_WARN(...)
+#define GN_ENGINE_DEBUG(...)
+#define GN_ENGINE_TRACE(...)
+#define GN_ENGINE_INFO(...)
+#define GN_ENGINE_ERROR(...)
 #endif
---
+++ src/Genesis/Core/Log.cpp
@@ -0,0 +1,19 @@
+#include "Genesis/Core/Log.hpp"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+
+namespace Genesis
+{
+std::shared_ptr<spdlog::logger> Logger::clientLog;
+std::shared_ptr<spdlog::logger> Logger::coreLog;
+
+void Logger::init()
+{
+    spdlog::set_pattern("%^[%n %l %D %X] %v%$");
+    clientLog = spdlog::stdout_color_mt("client");
+    clientLog->set_level(spdlog::level::trace);
+    coreLog = spdlog::stdout_color_mt("core");
+    coreLog->set_level(spdlog::level::trace);
+}
+}// namespace Genesis
