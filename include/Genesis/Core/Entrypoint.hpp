b3d53a197930f71a94ba904d6da4b92491f51e50
FlowingSkiff <boats303@gmail.com>
2022-03-01 21:48:02 -0700

    initialized logger in main

--- include/Genesis/Core/Entrypoint.hpp
+++ include/Genesis/Core/Entrypoint.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "Core/Core.hpp"
 #include "Core/Application.hpp"
-
+#include "Core/Log/Log.hpp"
 
 extern Genesis::Application* Genesis::CreateApplication();
 
 int main(int argc, char** argv)
 {
+    Genesis::Logger::init();
     auto* application = Genesis::CreateApplication();
     application->run();
     delete application;
