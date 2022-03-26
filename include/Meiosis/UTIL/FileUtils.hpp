c799e68cd88e0169cd7d964a76080af908166266
FlowingSkiff <boats303@gmail.com>
2022-03-26 12:43:02 -0600

    Fixed spelling mistake

--- include/Meiosis/UTIL/FileUtils.hpp
+++ include/Meiosis/UTIL/FileUtils.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include "Meiosis/Core/Core.hpp"
 #include <string>
 namespace Meiosis::Util
 {
-    std::string filenameFromPath(const std::string& file_path, bool remove_extension = false);
+    std::string ME_API filenameFromPath(const std::string& file_path, bool remove_extension = false);
 }
