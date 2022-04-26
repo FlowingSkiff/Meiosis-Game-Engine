12fe940266659d87eeb194583054ee378b97a802
FlowingSkiff <boats303@gmail.com>
2022-04-25 21:47:47 -0600

    changed to lib defined build

--- src/Meiosis/Core/Window.cpp
+++ src/Meiosis/Core/Window.cpp
@@ -1,6 +1,6 @@
 #include "Core/Window.hpp"
 
-#ifdef _WIN32
+#ifdef MEIOSIS_BUILD_WINDOWS
 #include "Platform/Windows/WindowsWindow.hpp"
 #else
 #error "Windows is only supported."
@@ -10,7 +10,7 @@ namespace Meiosis
 {
 std::unique_ptr<Window> Window::create(const std::string& name, uint32_t width, uint32_t height)
 {
-#ifdef _WIN32
+#ifdef MEIOSIS_BUILD_WINDOWS
     return std::make_unique<WindowsWindow>(name, width, height);
 #else
     return nullptr;
