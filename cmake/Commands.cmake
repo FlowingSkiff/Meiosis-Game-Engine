97d6a3fa42b382077e57d7c75f37a367693697bd
FlowingSkiff <boats303@gmail.com>
2022-04-27 22:22:18 -0600

    fixed cmake with msvc issue

--- cmake/Commands.cmake
+++ cmake/Commands.cmake
@@ -5,7 +5,7 @@ function(
 )
 string(REGEX REPLACE "[/\\]+$" "" folder ${folder})
 string(REGEX MATCH "[^/\\]+$" folder_name ${folder})
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+if(MSVC)
     add_custom_command(
         TARGET ${target} PRE_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
@@ -15,7 +15,7 @@ else()
     add_custom_command(
         TARGET ${target} PRE_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${folder}  ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}/${folder_name}
+        ${folder}  ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${folder_name}
     )
 endif()
 endfunction()
