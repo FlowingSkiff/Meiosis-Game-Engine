73158e0dba7381ea29f3e3fe0be24b9216bab883
FlowingSkiff <boats303@gmail.com>
2022-05-11 22:28:51 -0600

    removed nested namespace

--- include/Meiosis/Core/Input.hpp
+++ include/Meiosis/Core/Input.hpp
@@ -4,14 +4,11 @@
 #include "Meiosis/Core/MouseCodes.hpp"
 #include <glm/glm.hpp>
 
-namespace Meiosis
+namespace Meiosis::Input
 {
-namespace Input
-{
-    bool isKeyPressed(KeyCode key_code);
-    bool isMouseButtonPressed(MouseCode mouse_button);
-    auto getMousePosition() -> glm::vec2;
-    auto getMouseX() -> float;
-    auto getMouseY() -> float;
-}// namespace Input
-}// namespace Meiosis
+bool isKeyPressed(KeyCode key_code);
+bool isMouseButtonPressed(MouseCode mouse_button);
+auto getMousePosition() -> glm::vec2;
+auto getMouseX() -> float;
+auto getMouseY() -> float;
+}// namespace Meiosis::Input
